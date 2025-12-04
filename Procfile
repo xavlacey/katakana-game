b1f9a5554b9247627e6a8280b2a0a75ce3d2760a
Xav Lacey <x@vierlacey.com>
2025-12-04 22:33:40 +0000

    Add deployment configuration for Railway/Heroku/Render
    - Add requirements.txt with Django, gunicorn, and whitenoise
    - Add Procfile for web server configuration
    - Configure ALLOWED_HOSTS and DEBUG from environment variables
    - Add WhiteNoise for static file serving
    - Add runtime.txt to specify Python version

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python manage.py migrate && python manage.py load_katakana_words && gunicorn katakanagame.wsgi --log-file -
--- katakanagame/settings.py
+++ katakanagame/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5c6-j4!oss(84aw)!utw7*bjn9ld4xn**ym)!qyv3dw^d0b29&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+import os
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
